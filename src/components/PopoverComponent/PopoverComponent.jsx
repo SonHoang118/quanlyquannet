e80359ec246c66f11c4a56a9d0ea1fcb3a25a447
SonHoang118 <130476375+SonHoang118@users.noreply.github.com>
2024-05-21 15:18:21 +0700

    Update PopoverComponent.jsx

--- src/components/PopoverComponent/PopoverComponent.jsx
+++ src/components/PopoverComponent/PopoverComponent.jsx
@@ -6,7 +6,7 @@ const PopoverComponent = (props) => {
     const {handleLogout} = props
     return (
         <div className={styleModule.popover}>
-            <div><Link to={'/profile'}>Thông tin tài khoản</Link></div>
+            <div>Thông tin tài khoản</div>
             {/* <div><Link to={'/user-profile'}>Đơn hàng</Link></div> */}
             {/* <div>Đơn hàng</div> */}
             <div onClick={handleLogout}>Đăng xuất</div>
